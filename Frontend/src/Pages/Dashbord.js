886cb9918fdb50629c3ef3116de692a43becd274
MADUSHKA <kamadushkachathuranga@gmail.com>
2024-03-04 13:16:23 +0530

    create slide show

--- Frontend/src/Pages/Dashbord.js
+++ Frontend/src/Pages/Dashbord.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { Box } from "@mui/material";
+import "../Style/Style.css"
+
+
 
 export default function Dashbord() {
+ 
+  
   return (
-    <div>Dashbord</div>
-  )
+    <>
+    <Box sx={{padding:10, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      
+    {/* <Carousel className="main-slide" showThumbs={false} showStatus={false} style={{ maxWidth: '60%' }}> */}
+    <Carousel className="main-slide" autoPlay={true} infiniteLoop={true} showThumbs={false} showStatus={false} >
+                <div>
+                    <img src="https://cdn141.picsart.com/317513228192211.png?to=crop&type=webp&r=1456x1456&q=85" height="auto" width="200px" />
+                    
+                </div>
+                <div>
+                    <img src="https://cdn130.picsart.com/249619770004212.png?to=crop&type=webp&r=1456x1147&q=85" height="auto" width="200px" />
+                    
+                </div>
+                <div>
+                    <img src="https://cdn130.picsart.com/289328266010211.png?to=crop&type=webp&r=1456x712&q=85" height="auto" width="200px" />
+                    
+                </div>
+            </Carousel>
+
+            </Box>
+    
+    </>
+  );
 }
