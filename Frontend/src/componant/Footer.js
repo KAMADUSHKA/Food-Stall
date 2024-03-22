5c71771277764e36b9af6996eb9e1bad9c5e55ea
MADUSHKA <kamadushkachathuranga@gmail.com>
2024-03-22 11:07:18 +0530

    upgrade the new footer

--- Frontend/src/componant/Footer.js
+++ Frontend/src/componant/Footer.js
@@ -49,30 +49,30 @@ export default function Footer() {
     <>
       <AppBar position="static" sx={{ backgroundColor: "rgb(18, 104, 18)" }}>
         <Container maxWidth="xl">
-          <Toolbar disableGutters sx={{ marginBottom: 10 }}>
+          <Toolbar disableGutters sx={{ marginBottom: 2 }}>
             {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
             <Grid container>
               <Grid items xs={12}>
-                
-                <Box sx={{ flexGrow: 0 }}>
-                  <Tooltip title="Open settings">
-                    <IconButton sx={{ p: 0 }}>
-                      <Avatar
-                        alt="Remy Sharp"
-                        src="https://dsuabgmmtxmj1.cloudfront.net/companyweb/adeona_new_logo_circle.png"
-                      />
-                    </IconButton>
-                  </Tooltip>
-                </Box>
                 <Box
                   sx={{
                     display: "flex",
                     justifyContent: "space-between",
                     alignItems: "center",
-                    width: 1300,
+                    marginTop: 4,
+                    // width: 1300,
                   }}
                 >
-                  <Box sx={{ marginLeft: 2 }}>
+                  <Box sx={{ marginLeft: 2, display: "flex" }}>
+                    <Box sx={{ flexGrow: 0 }}>
+                      <Tooltip title="Open settings">
+                        <IconButton sx={{ p: 0 }}>
+                          <Avatar
+                            alt="Remy Sharp"
+                            src="https://dsuabgmmtxmj1.cloudfront.net/companyweb/adeona_new_logo_circle.png"
+                          />
+                        </IconButton>
+                      </Tooltip>
+                    </Box>
                     <Typography
                       variant="h6"
                       noWrap
@@ -81,6 +81,7 @@ export default function Footer() {
                       sx={{
                         mr: 2,
                         // display: { xs: 'none', md: 'flex' },
+                        marginLeft: 2,
                         display: "flex",
                         fontFamily: "monospace",
                         fontWeight: 700,
@@ -136,53 +137,95 @@ export default function Footer() {
                         </Typography>
                       </Box>
                     </Box>
-                    <Box>
-                      <Grid container>
-                        <Grid items xs={12}>
-                          <Typography
-                            variant="h6"
-                            noWrap
-                            component="a"
-                            href="/"
-                            sx={{
-                              display: { xs: "none", md: "flex" },
-
-                              fontFamily: "monospace",
-                              fontWeight: 700,
-                              letterSpacing: ".0.5rem",
-                              color: "inherit",
-                              textDecoration: "none",
-                              marginLeft: 0,
-                            }}
-                          >
-                            dfgsthdrtty
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    </Box>
                   </Box>
                 </Box>
-              </Grid> 
+              </Grid>
               <Grid items xs={12}>
-                          <Typography
-                            variant="h6"
-                            noWrap
-                            component="a"
-                            href="/"
-                            sx={{
-                              display: { xs: "none", md: "flex" },
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: 12,
+                    marginLeft: 4,
+                    // width: 1300,
+                  }}
+                >
+                  <Box>
+                  <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                      display: { xs: "none", md: "flex" },
+                      // display:"flex",
+                      fontFamily: "monospace",
+                      fontWeight: 700,
+                      letterSpacing: "0.1rem",
+                      color: "inherit",
+                      textDecoration: "none",
+                      marginLeft: 0,
+                      fontSize: 12,
+                    }}
+                  >
+                    @ 2023 Copyright: Adeona Technologies PVT (LTD) All Rights
+                    Reserved
+                  </Typography>
+                  </Box>
+                  <Box sx={{width:120, display:"flex" ,justifyContent:"space-between", }}>
+                  <AiFillFacebook />
 
-                              fontFamily: "monospace",
-                              fontWeight: 700,
-                              letterSpacing: ".0.5rem",
-                              color: "inherit",
-                              textDecoration: "none",
-                              marginLeft: 0,
-                            }}
-                          >
-                            dfgsthdrtty
-                          </Typography>
-                        </Grid>
+                  <AiOutlineTwitter />
+
+                  <AiOutlineInstagram />
+
+                  <AiFillLinkedin />
+                  </Box>
+                  <Box sx={{justifyContent:"space-between", display:"flex",  }}>
+                    <Box>
+                  <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                      display: { xs: "flex", md: "none" },
+                      // display:"flex",
+                      fontFamily: "monospace",
+                      fontWeight: 700,
+                      letterSpacing: ".0.5rem",
+                      color: "inherit",
+                      textDecoration: "none",
+                      marginLeft: 0,
+                    }}
+                  >
+                    Privacy Policy
+                  </Typography>
+                  </Box>
+                  <Box sx={{marginLeft:3}}>
+                  <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                      display: { xs: "flex", md: "none" },
+                      // display:"flex",
+                      fontFamily: "monospace",
+                      fontWeight: 700,
+                      letterSpacing: ".0.5rem",
+                      color: "inherit",
+                      textDecoration: "none",
+                      marginLeft: 0,
+                    }}
+                  >
+                    Terms
+                  </Typography>
+                  </Box>
+                  </Box>
+                </Box>
+              </Grid>
             </Grid>
           </Toolbar>
         </Container>
